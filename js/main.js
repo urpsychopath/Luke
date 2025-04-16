1a20130b50999fbfdd7887b3b2833612c27889d9
urpsychopath <urieslife@gmail.com>
2025-04-16 03:33:52 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+    
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+  });
+  
